5f8fe1f5cfe88b114ffd62e678a2d634233394e9
ipacmanx <ipacmanx@gmail.com>
2016-09-08 03:47:30 +0300

    slider-fix

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,5 @@
 $(function() {
+  $('.left-arrow').hide();
   $('.trainers-slider li').eq(0).addClass('visible');
 });
 
@@ -7,48 +8,33 @@ $(document).on('click','.arrow',function(){
   count = count-1;
   var i = $('.trainers-slider li.visible').index();
   var n = i+1;
-  $('.visible').fadeOut(function(){
-    $('.visible').removeClass('visible');
-  });
-  if ($(this).hasClass('left-arrow')) {
+
+  if (n == count ) {
+    $('.right-arrow').hide();
+  } else {
+    $('.right-arrow').show();
+  }
+
+  if ((i-1) == 0) {
+    $('.left-arrow').hide();
+  } else {
+    $('.left-arrow').show();
+  }
+
+  $('.visible').removeClass('visible');
+  if ($(this).hasClass('right-arrow')) {
+
     if (n > count) {
-      $('.trainers-slider li').eq(0).fadeIn(function(){
-        $('.trainers-slider li').eq(0).addClass('visible');
-      });
+      $('.trainers-slider li').eq(0).addClass('visible')
     } else {
-      $('.trainers-slider li').eq(n).fadeIn(function(){
-        $('.trainers-slider li').eq(n).addClass('visible');
-      });
+      $('.trainers-slider li').eq(n).addClass('visible')
     }
   } else {
     var n = i-1;
     if (n >= 0) {
-      $('.trainers-slider li').eq(n).fadeIn(function(){
         $('.trainers-slider li').eq(n).addClass('visible');
-      });
     } else {
-      $('.trainers-slider li').eq(count-1).fadeIn(function(){
         $('.trainers-slider li').eq(count-1).addClass('visible');
-      });
     }
-  }
-});
-
-$(document).on('click','.right-arrow',function(){
-  var count = $('.trainers-slider li').length;
-  count = count-1;
-  var i = $('.trainers-slider li.visible').index();
-  var n = i+1;
-  $('.visible').fadeOut(function(){
-    $('.visible').removeClass('visible');
-  });
-  if (n > count) {
-    $('.trainers-slider li').eq(0).fadeIn(function(){
-      $('.trainers-slider li').eq(0).addClass('visible');
-    });
-  } else {
-    $('.trainers-slider li').eq(n).fadeIn(function(){
-      $('.trainers-slider li').eq(n).addClass('visible');
-    });
   }
 });
